e1f1818b62dfce0d3c5aa02e53934831fa6559fd
asifhossian <m.asif41.001@gmail.com>
2020-05-07 22:30:10 +0600

    code done

---
+++ Quotient and Remainder.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int dividend, divisor, quotient, remainder;
+    printf("Enter dividend: ");
+    scanf("%d", &dividend);
+    printf("Enter divisor: ");
+    scanf("%d", &divisor);
+
+    if(divisor == 0)
+    {
+        printf("Undefined \nCan not divide by zero");
+        return 0;
+    }
+
+    // Computes quotient
+    quotient = dividend / divisor;
+
+    // Computes remainder
+    remainder = dividend % divisor;
+
+    printf("Quotient = %d\n", quotient);
+    printf("Remainder = %d", remainder);
+    return 0;
+}
